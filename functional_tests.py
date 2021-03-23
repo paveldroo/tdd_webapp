5588ca6a26be6ffe49ee6bc7c4380bd1a0bf9539
Pavel Druzhinin <paveldroo@gmail.com>
2021-03-23 19:32:32 +0300

    added FT

--- functional_tests.py
+++ functional_tests.py
@@ -39,9 +39,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # There is still a text box inviting ger to add another item. He enters "Use peacock feathers" to make a fly"
         self.fail('Finish the test!')
