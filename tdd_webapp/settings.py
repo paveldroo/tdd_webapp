2c421d3ba5a237145863c0912f3681e23c6c52c1
Pavel Druzhinin <paveldroo@gmail.com>
2021-04-09 14:56:13 +0300

    add new url to allowed_hosts

--- tdd_webapp/settings.py
+++ tdd_webapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'staging.paveldroo.xyz']
+ALLOWED_HOSTS = ['localhost', 'staging.paveldroo.xyz', 'paveldroo.xyz']
 
 
 # Application definition
