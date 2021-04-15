e6e2f05ecbf6a3692e0fbdb6632a091792f0d5bb
Pavel Druzhinin <paveldroo@gmail.com>
2021-04-15 18:00:17 +0300

    add jquery, qunit tests, list.js with keypress listeners

--- tdd_webapp/settings.py
+++ tdd_webapp/settings.py
@@ -123,4 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (BASE_DIR / 'static', )
