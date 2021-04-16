60a40f206e8b491105449b6894e0abb7a3d5fc94
Pavel Druzhinin <paveldroo@gmail.com>
2021-04-16 12:12:48 +0300

    add static root

--- tdd_webapp/settings.py
+++ tdd_webapp/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR / 'static', )
+if DEBUG:
+    STATICFILES_DIRS = (BASE_DIR / 'static',)
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+
+
