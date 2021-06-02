d1a2823a61e26d195d1db6af1b36d3d7d92e088f
Pavel Druzhinin <paveldroo@gmail.com>
2021-06-02 13:44:04 +0300

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,9 +3,14 @@ import time
 from datetime import datetime
 
 from django.conf import settings
+from django.contrib.auth import SESSION_KEY, BACKEND_SESSION_KEY
+from django.contrib.sessions.backends.db import SessionStore
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+
+from accounts.models import User
 
 MAX_WAIT = 10
 SCREEN_DUMP_LOCATION = settings.BASE_DIR / 'functional_tests/screendumps'
@@ -84,3 +89,27 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        # to set a cookie we need to first visit the domain
+        # 404 pages load the quickest!
+        self.browser.get(self.live_server_url + '/404_no_such_url')
+        self.browser.add_cookie(
+            dict(
+                name=settings.SESSION_COOKIE_NAME,
+                value=session.session_key,
+                path='/'
+            )
+        )
+
+    def add_list_item(self, item_text):
+        num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
